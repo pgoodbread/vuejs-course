6a6a0811199d0f23da1a4a33b9e0545930a15b67
Philip Gutbrodt <philip.gutbrodt@spring-media.de>
2019-11-18 10:13:20 +0100

    feat: start component assignment

---
+++ vuedemy/src/ServerGroup.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <ServerInstance v-for="index in 5" :key="index"  />
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import ServerInstance from './ServerInstance'
+export default {
+  component: {
+    ServerInstance
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuedemy/src/ServerInstance.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>
+    Server #{{ key }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    key: {
+      type: Number,
+      default: () => 0
+    }
+  }, 
+  data() {
+    return { key: 0 }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
