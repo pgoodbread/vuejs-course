e2391f7cc183f3400a430cc296780fbae5632770
Philip Gutbrodt <philip.gutbrodt@spring-media.de>
2020-01-19 12:42:55 +0100

    feat: dynamic components and lifecycle hooks

--- vuedemy/src/ex_quotes/App.vue
+++ vuedemy/src/ex_quotes/App.vue
@@ -2,15 +2,32 @@
  <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <Quote>
+        <button @click="selectedComponent = 'Quote'">Quote</button>
+        <button @click="selectedComponent = 'Author'">Author</button>
+        <button @click="selectedComponent = 'New'">New Quote</button>
+        <p>{{ selectedComponent }}</p>
+        <hr>
+        <keep-alive>
+          <component :is="selectedComponent">
+            <template v-slot:title>
+              <h2>Quote</h2>
+            </template>
+            <template v-slot:content>
+              <p>A wonderful quote</p>
+            </template>
+            <template v-slot>
+              <h2>default content</h2>
+            </template>
+          </component>
+        </keep-alive>
+        <!-- <Quote>
           <template v-slot:title>
             <h2>Quote</h2>
           </template>
           <template v-slot:content>
             <p>A wonderful quote</p>
           </template>
-
-        </Quote>
+        </Quote> -->
       </div>
     </div>
   </div>
@@ -19,10 +36,20 @@
 
 <script>
 import Quote from './Quote/Quote'
+import Author from './Quote/Author'
+import New from './Quote/New'
 
 export default {
     components: {
-        Quote
+        Quote,
+        Author,
+        New
+    }, 
+    data() {
+      return {
+        quoteTitle: 'Bla',
+        selectedComponent: 'Quote'
+      }
     }
 }
 </script>
---
+++ vuedemy/src/ex_quotes/Quote/New.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+   <h3>New Quote</h3>
+   <h4>{{ counter }}</h4>
+   <button @click="counter++">Increase</button>
+ </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    }
+  }, 
+  deactivated() {
+    console.log('deactivated')
+  },
+  activated() {
+    console.log('activated')
+  }
+}
+</script>
+
+<style scoped>
+</style>
