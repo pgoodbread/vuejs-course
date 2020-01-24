eb8eb04b47a2b470afc97f7ffd3a7cf106e3d8ba
Philip Gutbrodt <philip.gutbrodt@spring-media.de>
2020-01-24 13:51:11 +0100

    feat: finish forms exercise

--- vuedemy/src/ex_forms2/App.vue
+++ vuedemy/src/ex_forms2/App.vue
@@ -1,46 +1,106 @@
 <template>
     <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
+        <!-- <template v-if="!submitted"> -->
+        <template>
+            <form>
+                <FullName v-model="userData.name"></FullName>
+                        <!-- Exercise 1 -->
+                        <!-- Create a Signup Form where you retrieve the following Information -->
+                        <!-- Full Name (First Name + Last Name) -->
+                        <!-- Mail -->
+                        <!-- Password -->
+                        <!-- Store Data? Yes/No -->
 
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <div class="form-group">
+                            <label for="email">Mail</label>
+                            <input
+                                    type="text"
+                                    id="email"
+                                    class="form-control" 
+                                    v-model="userData.email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input
+                                    type="password"
+                                    id="password"
+                                    class="form-control"
+                                    v-model="userData.password">
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </form>
-        <hr>
-        <div class="row">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                        <label for="male">
+                            <input
+                                    type="radio"
+                                    id="male"
+                                    value="true"
+                                    v-model="storeData"> Yes
+                        </label>
+                        <label for="female">
+                            <input
+                                    type="radio"
+                                    id="female"
+                                    value="false"
+                                    v-model="storeData"> No
+                        </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <button
+                                class="btn btn-primary"
+                                @click.prevent="submitted = true">Submit!
+                        </button>
+                    </div>
+                </div>
+                        <!-- Exercise 3 -->
+                        <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                        <!-- which still holds the First Name and Last Name Input Field -->
+            </form>
+        </template>
+        <template>
+            <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ userData.name }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
                     </div>
                 </div>
             </div>
         </div>
+        </template>
+        
     </div>
 </template>
 
 <script>
+import FullName from './FullName'
     export default {
+        components: {
+           FullName,
+        },
+        data() {
+            return {
+                userData: {
+                    name: 'Philip Gutbrodt',
+                    email: '',
+                    password: '',
+                },
+                storeData: true,
+                submitted: false,
+                
+            }
+        }
     }
 </script>
 
---
+++ vuedemy/src/ex_forms2/FullName.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <div class="form-group">
+        <label for="name">First Name</label>
+        <input
+          type="text"
+          id="name"
+          class="form-control" 
+          :value="first"
+          @input="update(true, $event)">
+      </div>
+      <div class="form-group">
+        <label for="name">Last Name</label>
+        <input
+          type="text"
+          id="name"
+          class="form-control" 
+          :value="last"
+          @input="update(false,$event)">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  computed: {
+    first() {
+      return this.value.split(' ')[0]
+    },
+    last() {
+      return this.value.split(' ')[1]
+    }
+  },
+  methods: {
+    update(isFirst, event) {
+      let name = ''
+      if (isFirst) {
+        name = event.target.value + ' ' + this.last
+      } else {
+        name = this.first + ' ' + event.target.value
+      }
+      this.value = name
+      this.$emit('input', name)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
