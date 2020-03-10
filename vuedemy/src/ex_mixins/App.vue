0067f08800ac3134a017d1098e61cb483d1b9e2e
Philip Gutbrodt <philip.gutbrodt@spring-media.de>
2020-03-10 08:44:15 +0100

    feat: add exercise

--- vuedemy/src/ex_mixins/App.vue
+++ vuedemy/src/ex_mixins/App.vue
@@ -1,24 +1,29 @@
 <template>
     <div class="container">
-        <input v-model="filteredText">
-        <ul>
-          <li v-for="(fruit, index) in filteredFruits" :key="index">{{fruit}}</li>
-        </ul>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <!-- Exercise 1) -->
+                <!-- Build a local Filter which reverses the Text it is applied on -->
+
+                <!-- Exercise 2 -->
+                <!-- Build a global Filter which counts the length of a word and it appends it -->
+                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+
+                <!-- Exercise 3 -->
+                <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+
+                <!-- Exercise 4 -->
+                <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import { fruitMixin } from './filteredFruitsMixin'
     export default {
-        components: {
-        },
-        mixins: [fruitMixin]
     }
 </script>
 
 <style>
-#test123 {
-    cursor: pointer;
-    user-select: none;
-}
 </style>
